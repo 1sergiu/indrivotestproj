cfb1cc4db3f29ecb5cfd23dc634a96d5978461fe
unknown <i.sergiu@gmx.com>
2023-12-25 11:39:05 +0200

    Add Exception Handling for Entity Operations

--- Controllers/EntityController.cs
+++ Controllers/EntityController.cs
@@ -3,6 +3,7 @@ using IndrivoTestProj.Data.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Threading.Tasks;
+using IndrivoTestProj.Exceptions;
 
 namespace IndrivoTestProj.Controllers
 {
@@ -27,29 +28,61 @@ namespace IndrivoTestProj.Controllers
         [HttpGet("{guid}")]
         public async Task<IActionResult> GetEntityById(Guid guid)
         {
-            var entity = await _entityService.GetEntityByIdAsync(guid);
-            return entity != null ? Ok(entity) : NotFound();
+            try
+            {
+                var entity = await _entityService.GetEntityByIdAsync(guid);
+                return entity != null ? Ok(entity) : NotFound();
+            }
+            catch (GuidNotFoundException ex)
+            {
+                return BadRequest($"{ex.Message}");
+            }
+
         }
 
         [HttpPost]
         public async Task<IActionResult> AddEntity([FromBody] EntityVM entityVM)
         {
-            await _entityService.AddEntityAsync(entityVM);
-            return CreatedAtAction(nameof(GetEntityById), new { guid = Guid.NewGuid() }, entityVM);
+            try
+            {
+                await _entityService.AddEntityAsync(entityVM);
+                return CreatedAtAction(nameof(GetEntityById), new { guid = Guid.NewGuid() }, entityVM);
+            }
+            catch (GuidNotFoundException ex)
+            {
+                return BadRequest($"{ex.Message}");
+            }
+
         }
 
         [HttpPut("{guid}")]
         public async Task<IActionResult> UpdateEntityById(Guid guid, [FromBody] EntityVM entityVM)
         {
-            var updatedEntity = await _entityService.UpdateEntityByIdAsync(guid, entityVM);
-            return updatedEntity != null ? Ok(updatedEntity) : NotFound();
+            try
+            {
+                var updatedEntity = await _entityService.UpdateEntityByIdAsync(guid, entityVM);
+                return updatedEntity != null ? Ok(updatedEntity) : NotFound();
+            }
+            catch (GuidNotFoundException ex)
+            {
+                return BadRequest($"{ex.Message}");
+            }
+
         }
 
         [HttpDelete("{guid}")]
         public async Task<IActionResult> DeleteEntityById(Guid guid)
         {
-            await _entityService.DeleteEntityByIdAsync(guid);
-            return NoContent();
+            try
+            {
+                await _entityService.DeleteEntityByIdAsync(guid);
+                return NoContent();
+            }
+            catch (GuidNotFoundException ex)
+            {
+                return BadRequest($"{ex.Message}");
+            }
         }
     }
+
 }
--- Data/Services/EntityService.cs
+++ Data/Services/EntityService.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IndrivoTestProj.Exceptions;
 
 namespace IndrivoTestProj.Data.Services
 {
@@ -19,11 +20,9 @@ namespace IndrivoTestProj.Data.Services
 
         public async Task AddEntityAsync(EntityVM entity, CancellationToken cancellationToken = default)
         {
-            //Verifing if Classfier exists
-            var classifierExists = await _context.Classifiers.AnyAsync(c => c.Guid == entity.TypeGuid);
-            if (!classifierExists)
+            if (!await ClassifierExistsAsync(entity.TypeGuid, cancellationToken))
             {
-                throw new InvalidOperationException("Classifier with this TypeGuid not found");
+                throw new GuidNotFoundException($"Classifier with id: {entity.TypeGuid} not found.");
             }
 
             var _entity = new Entity
@@ -41,28 +40,56 @@ namespace IndrivoTestProj.Data.Services
             => await _context.Entities.ToListAsync(cancellationToken);
 
         public async Task<Entity> GetEntityByIdAsync(Guid guid, CancellationToken cancellationToken = default)
-            => await _context.Entities.FirstOrDefaultAsync(n => n.Guid == guid, cancellationToken);
-
-        public async Task<Entity> UpdateEntityByIdAsync(Guid entityId, EntityVM entity, CancellationToken cancellationToken = default)
         {
-            var _entity = await _context.Entities.FirstOrDefaultAsync(c => c.Guid == entityId, cancellationToken);
-            if (_entity != null)
+            var _entity = await _context.Entities.FirstOrDefaultAsync(n => n.Guid == guid, cancellationToken);
+
+            if (_entity == null)
             {
-                _entity.Title = entity.Title;
-                _entity.Description = entity.Description;
-                _entity.TypeGuid = entity.TypeGuid;
-                await _context.SaveChangesAsync(cancellationToken);
+                throw new GuidNotFoundException($"Entity with id: {guid} not found");
             }
             return _entity;
         }
+        public async Task<Entity> UpdateEntityByIdAsync(Guid entityId, EntityVM entity, CancellationToken cancellationToken = default)
+        {
+            if (!await EntityExistsAsync(entityId, cancellationToken))
+            {
+                throw new GuidNotFoundException($"Entity with id: {entityId} not found");
+            }
+
+            if (!await ClassifierExistsAsync(entity.TypeGuid, cancellationToken))
+            {
+                throw new GuidNotFoundException($"Classifier with id: {entity.TypeGuid} not found.");
+            }
+
+            var _entity = await _context.Entities.FirstOrDefaultAsync(c => c.Guid == entityId, cancellationToken);
+
+            _entity.Title = entity.Title;
+            _entity.Description = entity.Description;
+            _entity.TypeGuid = entity.TypeGuid;
+            await _context.SaveChangesAsync(cancellationToken);
+            return _entity;
+        }
+
         public async Task DeleteEntityByIdAsync(Guid entityId, CancellationToken cancellationToken = default)
         {
-            var _entity = await _context.Entities.FirstOrDefaultAsync(n => n.Guid == entityId, cancellationToken);
-            if (_entity != null)
+            if (!await EntityExistsAsync(entityId, cancellationToken))
             {
-                _context.Entities.Remove(_entity);
-                await _context.SaveChangesAsync(cancellationToken);
+                throw new GuidNotFoundException($"Entity with id: {entityId} not found");
             }
+
+            var _entity = await _context.Entities.FirstOrDefaultAsync(n => n.Guid == entityId, cancellationToken);
+            _context.Entities.Remove(_entity);
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        private async Task<bool> EntityExistsAsync(Guid entityId, CancellationToken cancellationToken = default)
+        {
+            return await _context.Entities.AnyAsync(n => n.Guid == entityId, cancellationToken);
+        }
+
+        private async Task<bool> ClassifierExistsAsync(Guid typeGuid, CancellationToken cancellationToken = default)
+        {
+            return await _context.Classifiers.AnyAsync(c => c.Guid == typeGuid, cancellationToken);
         }
     }
 }
